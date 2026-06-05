8273e0cd15d8ca4593d715712341a73525ae5a7b
cassianoomotta <cassianoomotta@gmail.com>
2026-06-05 18:20:02 -0300

    fix: refina proporcoes de braço superior deltoide e antebraco no MuscleMap

--- src/components/MuscleMap.tsx
+++ src/components/MuscleMap.tsx
@@ -33,7 +33,7 @@ export default function MuscleMap({ muscleGroup, className = "", size = 200 }: M
   const highlightCalves = isHighlighted(["PANTURRILHA", "PANTURRILHAS", "CALF", "CALVES"]);
   const highlightCardio = isHighlighted(["CARDIO", "AQUECIMENTO", "MOBILIDADE", "WARMUP"]);
 
-  // Cores Tailwind Dinâmicas baseadas na TechFitness (Azul Vibrante e contornos limpos)
+  // Cores Tailwind Dinâmicas baseadas na TechFitness (Azul Vibrante)
   const getFillClass = (highlight: boolean) => {
     if (highlightCardio) {
       return "fill-blue-400 dark:fill-blue-500/80 stroke-blue-600 dark:stroke-blue-400 drop-shadow-[0_0_4px_rgba(59,130,246,0.3)] transition-all duration-500";
@@ -62,24 +62,24 @@ export default function MuscleMap({ muscleGroup, className = "", size = 200 }: M
           <polygon points="46,28 54,28 53,35 47,35" className={neutroClass} />
 
           {/* Ombros (Deltoides) */}
-          <path d="M 35,35 C 30,37 27,42 27,52 C 27,58 29,60 32,58 C 34,53 34,44 35,35 Z" className={getFillClass(highlightShoulders)} />
-          <path d="M 65,35 C 70,37 73,42 73,52 C 73,58 71,60 68,58 C 66,53 66,44 65,35 Z" className={getFillClass(highlightShoulders)} />
+          <path d="M 35,35 C 31,37 28,41 28,50 C 28,52 30,52 32,50 C 33,46 34,42 35,35 Z" className={getFillClass(highlightShoulders)} />
+          <path d="M 65,35 C 69,37 72,41 72,50 C 72,52 70,52 68,50 C 67,46 66,42 65,35 Z" className={getFillClass(highlightShoulders)} />
 
           {/* Peito (Peitorais) */}
           <path d="M 50,35 L 35,38 C 34,47 34,55 37,61 C 42,60 46,58 50,55 Z" className={getFillClass(highlightChest)} />
           <path d="M 50,35 L 65,38 C 66,47 66,55 63,61 C 58,60 54,58 50,55 Z" className={getFillClass(highlightChest)} />
 
           {/* Braços (Bíceps na Frente) */}
-          <path d="M 32,58 C 30,61 29,66 30,76 C 31,79 34,79 35,76 C 36,68 35,61 32,58 Z" className={getFillClass(highlightBiceps)} />
-          <path d="M 68,58 C 70,61 71,66 70,76 C 69,79 66,79 65,76 C 64,68 64,61 68,58 Z" className={getFillClass(highlightBiceps)} />
+          <path d="M 32,50 C 29,56 28,68 29,82 C 31,84 34,84 35,82 C 35,70 34,58 32,50 Z" className={getFillClass(highlightBiceps)} />
+          <path d="M 68,50 C 71,56 72,68 71,82 C 69,84 66,84 65,82 C 65,70 66,58 68,50 Z" className={getFillClass(highlightBiceps)} />
 
           {/* Antebraços */}
-          <path d="M 30,76 L 24,111 C 25,113 27,113 28,111 L 35,76 Z" className={getFillClass(highlightForearms)} />
-          <path d="M 70,76 L 76,111 C 75,113 73,113 72,111 L 65,76 Z" className={getFillClass(highlightForearms)} />
+          <path d="M 29,82 L 24,112 C 26,114 27,114 28,112 L 35,82 Z" className={getFillClass(highlightForearms)} />
+          <path d="M 71,82 L 76,112 C 74,114 73,114 72,112 L 65,82 Z" className={getFillClass(highlightForearms)} />
 
           {/* Mãos */}
-          <path d="M 24,111 C 22,118 23,124 26,124 C 28,122 28,114 28,111 Z" className={neutroClass} />
-          <path d="M 76,111 C 78,118 77,124 74,124 C 72,122 72,114 72,111 Z" className={neutroClass} />
+          <path d="M 24,112 C 22,118 23,122 25,122 C 27,120 28,114 28,112 Z" className={neutroClass} />
+          <path d="M 76,112 C 78,118 77,122 75,122 C 73,120 72,114 72,112 Z" className={neutroClass} />
 
           {/* Core (Abdômen / Oblíquos) */}
           <path d="M 37,61 C 42,60 46,58 50,55 C 54,58 58,60 63,61 L 61,96 L 39,96 Z" className={getFillClass(highlightAbs)} />
@@ -129,8 +129,8 @@ export default function MuscleMap({ muscleGroup, className = "", size = 200 }: M
           <polygon points="46,28 54,28 53,35 47,35" className={neutroClass} />
 
           {/* Ombros (Deltoides Posteriores) */}
-          <path d="M 35,35 C 30,37 27,42 27,52 C 27,58 29,60 32,58 C 34,53 34,44 35,35 Z" className={getFillClass(highlightShoulders)} />
-          <path d="M 65,35 C 70,37 73,42 73,52 C 73,58 71,60 68,58 C 66,53 66,44 65,35 Z" className={getFillClass(highlightShoulders)} />
+          <path d="M 35,35 C 31,37 28,41 28,50 C 28,52 30,52 32,50 C 33,46 34,42 35,35 Z" className={getFillClass(highlightShoulders)} />
+          <path d="M 65,35 C 69,37 72,41 72,50 C 72,52 70,52 68,50 C 67,46 66,42 65,35 Z" className={getFillClass(highlightShoulders)} />
 
           {/* Costas Superior (Trapézio e Redondos) */}
           <path d="M 50,30 L 35,38 L 38,52 L 50,44 Z" className={getFillClass(highlightUpperBack)} />
@@ -143,16 +143,16 @@ export default function MuscleMap({ muscleGroup, className = "", size = 200 }: M
           <path d="M 39.5,68 L 60.5,68 L 59,82 L 41,82 Z" className={getFillClass(highlightLombar)} />
 
           {/* Braços (Tríceps nas Costas) */}
-          <path d="M 32,58 C 30,61 29,66 30,76 C 31,79 34,79 35,76 C 36,68 35,61 32,58 Z" className={getFillClass(highlightTriceps)} />
-          <path d="M 68,58 C 70,61 71,66 70,76 C 69,79 66,79 65,76 C 64,68 64,61 68,58 Z" className={getFillClass(highlightTriceps)} />
+          <path d="M 32,50 C 29,56 28,68 29,82 C 31,84 34,84 35,82 C 35,70 34,58 32,50 Z" className={getFillClass(highlightTriceps)} />
+          <path d="M 68,50 C 71,56 72,68 71,82 C 69,84 66,84 65,82 C 65,70 66,58 68,50 Z" className={getFillClass(highlightTriceps)} />
 
           {/* Antebraços */}
-          <path d="M 30,76 L 24,111 C 25,113 27,113 28,111 L 35,76 Z" className={getFillClass(highlightForearms)} />
-          <path d="M 70,76 L 76,111 C 75,113 73,113 72,111 L 65,76 Z" className={getFillClass(highlightForearms)} />
+          <path d="M 29,82 L 24,112 C 26,114 27,114 28,112 L 35,82 Z" className={getFillClass(highlightForearms)} />
+          <path d="M 71,82 L 76,112 C 74,114 73,114 72,112 L 65,82 Z" className={getFillClass(highlightForearms)} />
 
           {/* Mãos */}
-          <path d="M 24,111 C 22,118 23,124 26,124 C 28,122 28,114 28,111 Z" className={neutroClass} />
-          <path d="M 76,111 C 78,118 77,124 74,124 C 72,122 72,114 72,111 Z" className={neutroClass} />
+          <path d="M 24,112 C 22,118 23,122 25,122 C 27,120 28,114 28,112 Z" className={neutroClass} />
+          <path d="M 76,112 C 78,118 77,122 75,122 C 73,120 72,114 72,112 Z" className={neutroClass} />
 
           {/* Glúteos */}
           <path d="M 39,82 L 50,82 L 50,98 C 45,98 42,94 41,92 Z" className={getFillClass(highlightGlutes)} />
