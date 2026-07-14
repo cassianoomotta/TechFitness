d0dffbfefd84bad64cb7f61227645321c38d1eaa
cassianoomotta <cassianoomotta@gmail.com>
2026-07-13 22:39:52 -0300

    refactor: clean up project documentation and reorganize static assets by moving the logo to the public directory

--- refactor_dashboard.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const pagePath = path.join(__dirname, 'src/app/student/dashboard/page.tsx');
-let content = fs.readFileSync(pagePath, 'utf8');
-
-console.log("Refactoring page.tsx...");
